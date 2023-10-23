4d763b22aa69ffb0ed5f560b96a05297efea9393
Kobe Neilson <kobeneilson@gmail.com>
2023-10-22 21:32:43 -0700

    Created a working sliding bar that interacts with button
    The sliding bar will now create the sqaure number of its value divs in a single column, need to figure out how to make it into a canvas

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,14 @@ const canvasDiv = document.querySelector('.canvas');
 const inputSlider = document.querySelector('.js-slider-input');
 const testButton = document.querySelector('.test-button');
 
+
+
 testButton.addEventListener('click', () => {
+    canvasDiv.innerHTML = '';
     for (let i=0; i <= inputSlider.value * inputSlider.value; i++) {
         const colorDiv = document.createElement('div');
         colorDiv.classList.add('color-div');
-        canvasDiv.appendChild('colorDiv');
+        canvasDiv.appendChild(colorDiv);
     };
 
 });
