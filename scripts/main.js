5238388c67ee4e20f328c62217eb327382fd9cf2
Kobe Neilson <kobeneilson@gmail.com>
2023-10-26 14:49:28 -0700

    Finnally got the grid working
    I finally managed to get something resebling the final product working. There is a grid whose columns and rows make a square. I used a loop to create columns, then a subloop to create rows. Can beleive it worked.

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,25 @@ const inputSlider = document.querySelector('.js-slider-input');
 const testButton = document.querySelector('.test-button');
 
 
-
 testButton.addEventListener('click', () => {
     canvasDiv.innerHTML = '';
-    for (let i=0; i < inputSlider.value * inputSlider.value; i++) {
-        const colorDiv = document.createElement('div');
-        colorDiv.classList.add('color-div');
-        canvasDiv.appendChild(colorDiv);
+    const gridNumber = inputSlider.value;
+    for (let i=0; i < gridNumber; i++) {
+        const columnDiv = document.createElement('div');
+        columnDiv.classList.add('column-div');
+        columnDiv.classList.add(`column-${i}`);
+        canvasDiv.appendChild(columnDiv);
+
+        for (let index=0; index < gridNumber; index++) {
+            const colorCreateDiv = document.createElement('div');
+            colorCreateDiv.classList.add('color-div');
+            colorCreateDiv.classList.add(`row-${index}`);
+            columnDiv.appendChild(colorCreateDiv);
+        };
     };
 });
+
+const colorDiv = document.querySelector('.color-div');
+colorDiv.addEventListener('click', (event) => {
+    console.log(event.classList);
+})
