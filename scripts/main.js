aa33a6079dc64456e1022b7184e1429ad63dde7a
Kobe Neilson <kobeneilson@gmail.com>
2023-10-27 17:33:57 -0700

    Added a color selector
    Used the built in html color selector then saved the value ina  variable. That was then used to change the color of the cells. I also moved some things into functions so that the grid will load in when the page loads and so that the slider will have its own event listener.

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,35 @@
 const canvasDiv = document.querySelector('.canvas');
 const inputSlider = document.querySelector('.js-slider-input');
-const testButton = document.querySelector('.test-button');
+const clearButton = document.querySelector('.test-button');
 
+addGrid();
 
-testButton.addEventListener('click', () => {
+clearButton.addEventListener('click', () => {
+    addGrid();
+});
+
+inputSlider.addEventListener('mouseup', () => {
+    addGrid();
+})
+
+function addGrid() {
     canvasDiv.innerHTML = '';
     const gridNumber = inputSlider.value;
     for (let i=0; i < gridNumber; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.classList.add('row-div');
         canvasDiv.appendChild(rowDiv);
+
         for (let index=0; index < gridNumber; index++) {
             const colorDiv = document.createElement('div');
             colorDiv.classList.add('color-div');
             rowDiv.appendChild(colorDiv);
 
             colorDiv.addEventListener('click', (event) => {
-                
-            })
+                    const colorPicker = document.querySelector('.js-color-picker').value;
+                    event.target.style.vackgroundColor = 'white';
+                    event.target.style.backgroundColor = `${colorPicker}`;
+            });
         };
     };
-});
+}
