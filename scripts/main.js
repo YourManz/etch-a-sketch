bbbd9d798d30e5ed2a457eb9d9869f89280772e3
Kobe Neilson <kobeneilson@gmail.com>
2023-10-29 23:09:28 -0700

    I attempted to add rainbow mode
    I added some functions that in theory should help me to use rainbow mode, however I am not sure that they are functioning as expected.

--- scripts/main.js
+++ scripts/main.js
@@ -43,7 +43,37 @@ function addGrid() {
 }
 
 function addColor(event) {
+    if (rainbowMode) {
+        pickRainbow(event)
+    }
     const colorPicker = document.querySelector('.js-color-picker').value;
     event.target.style.vackgroundColor = 'white';
     event.target.style.backgroundColor = `${colorPicker}`;
+}
+
+let rainbowMode = false;
+
+const rainbowButton = document.querySelector('.js-rainbow-mode');
+rainbowButton.addEventListener('click', (event) => {
+    toggleButton(event);
+    if (rainbowMode) {
+        rainbowMode = false;
+    } else if (!rainbowMode) {
+        rainbowMode = true;
+    }
+    console.log(rainbowMode)
+});
+
+function pickRainbow(event) {
+    const colorPicker = document.querySelector('.js-color-picker').value;
+    event.target.style.vackgroundColor = 'white';
+    event.target.style.backgroundColor = `rgb(${randomNumber(0, 255)}, ${randomNumber(0, 255)}, ${randomNumber(0, 255)})`;
+};
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+};
+
+function toggleButton(event) {
+    event.target.classList.toggle('.toggle-button');
 }
